67ed7111e64a860deab9170817c9e6f88e46a90f
Maajoo <mio.kantola@hotmail.com>
2024-04-30 21:15:55 +0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
-
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Home from './components/Home.jsx';
 import Training from './components/Training.jsx';
 import Customer from './components/Customer.jsx';
 import Calendar from './components/Calendar.jsx'
 
-const router = createBrowserRouter([  // Import components that are used in routes
-  {
-    path: "/",
-    element: <App />,
-    children: [                       // children are nested routes with a route
-      {
-        element: <Home />,
-        index: true                   // index route does not need any path
-      },
-      {
-        path: "customer",                // path can be defined relative to the parent path
-        element: <Customer />,
-      },
-      {
-        path: "training",
-        element: <Training />,
-      },
-      {
-        path: "calendar",
-        element: <Calendar />,
-      },
-    ]
-  }
-]);
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
-    <RouterProvider router={router} />
+    <App />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/customer" component={Customer} />
+      <Route path="/training" component={Training} />
+      <Route path="/calendar" component={Calendar} />
+    </Switch>
   </HashRouter>,
 )
