22d1f2a830190da2c75c856760a1572c876d3784
Maajoo <mio.kantola@hotmail.com>
2024-04-30 21:05:37 +0300

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,36 @@ import './App.css'
 function App() {
 
 
+
   return (
-    <>
-      <div style={{ backgroundColor: "black", color: "white", padding: 20, font: "caption", fontSize: 30 }}>
-        <h1>Prime Peak</h1>
-        <h3>Personal training</h3>
-      </div>
-      <nav style={{ font: "caption" }}>
-        <Link to={"/"}>Home</Link>{' '}
-        <Link to={"/customer"}>Customer</Link>{' '}
-        <Link to={"/training"}>Training</Link>{' '}
-        <Link to={"/calendar"}>Calendar</Link>{' '}
-      </nav>
-      <Outlet />
-    </>
-  )
+    <div className="App">
+      <center>
+        <ul>
+          <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/customer"}>Customer</Link></li>
+        <li><Link to={"/training"}>Training</Link></li>
+        <li><Link to={"/calendar"}>Calendar</Link></li>
+      </ul>
+    </center>
+        </div >
+    )
 }
 
-export default App
+//   return (
+//     <>
+//       <div style={{ backgroundColor: "black", color: "white", padding: 20, font: "caption", fontSize: 30 }}>
+//         <h1>Prime Peak</h1>
+//         <h3>Personal training</h3>
+//       </div>
+//       <nav style={{ font: "caption" }}>
+//         <Link to={"/"}>Home</Link>{' '}
+//         <Link to={"/customer"}>Customer</Link>{' '}
+//         <Link to={"/training"}>Training</Link>{' '}
+//         <Link to={"/calendar"}>Calendar</Link>{' '}
+//       </nav>
+//       <Outlet />
+//     </>
+//   )
+// }
+
+// export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+
 
 import Home from './components/Home.jsx';
 import Training from './components/Training.jsx';
@@ -36,7 +38,7 @@ const router = createBrowserRouter([  // Import components that are used in rout
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <HashRouter>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </HashRouter>,
 )
