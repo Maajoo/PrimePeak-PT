436f2fdf0b13ec1544e2da70711cace152cd89bc
Maajoo <mio.kantola@hotmail.com>
2024-05-04 15:00:31 +0300

    käyttöönotto

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
-
 // // https://vitejs.dev/config/
 // export default defineConfig({
-//   base: '/reactapp/',
 //   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//   },
 // })
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/reactapp/',
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
