4a43a055c5a82c8977cbab5bf01f30f3addc0fd7
Maajoo <mio.kantola@hotmail.com>
2024-05-04 15:18:21 +0300

    käyttöönotto pt.2

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactapp/',
+  base: '/primepeak-pt/',
   plugins: [react()],
   test: {
     globals: true,
